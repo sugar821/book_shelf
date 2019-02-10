98df7d6628e3ea5ea1fee93c587491cbc12a938a
sugar821 <aug.sugar@gmail.com>
2019-02-10 18:33:54 +0900

    add login not finished

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,7 +42,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :price,
                                  :publish_date, :description,
-                                 :new_image)
+                                 :description, :new_image, :category_id)
   end
 
   def set_book
